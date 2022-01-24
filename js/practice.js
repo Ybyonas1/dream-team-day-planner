3aebb81d66783e6b8151d2213b174c44c0a53a9c
Yonas Woldemichael <ybyonas@gmail.com>
2022-01-24 10:04:11 -0500

    final commits

--- js/practice.js
+++ js/practice.js
@@ -3,75 +3,11 @@ var workH = 9;
 var today = moment().format("dddd, MMM Do, YYYY");
 var enteredText = $(".edit");
 
-var a = [];
-
 $("#currentDay").text(today);
-
-
-/**
- * --------- STYLING TIME BLOCKS----------
- * 
- * 
- * 1) Get current Hour. -Check-
- * 
- * 2) Determine styling for each time block
- *      If the current hour is greater than the time block hour, 
- *      style as .past
- * 
- *      If current hour is equivalant to time block hour, style
- *      as .present
- * 
- *      If current hour is less than the block hour, style as
- *      .future
- * 
- *  ---------- LOADING SAVED DATA -----------
- * 
- *
- * 1) Get any data from local storage
- *  2) Pull data form local storage into textarea
- * 
- * Whenever (current hour - 9) = (id) row == green
- * 
- *  * Whenever (current hour - 9) < (id) row == blue
- * 
- *  * Whenever (current hour - 9) > (id) row == green
- * 
- */
-
-// const textRow = document.getElementsByClass("description");
-
-function checkTime(event) {
-    var currentHour = moment().format('H') - 16;
-    var blockHour = event.target.dataset.id
-    console.log(currentHour);
-    console.log(blockHour);
-    if (currentHour == blockHour) {
-         document.getElementsByClass('description').style.backgoundColor = "Green";
-        //  setColor(textRow, "blue");
-        //   $('.description').css('background-color', 'Blue');
-     } else if (currentHour > blockHour) {
-         document.getElementsByClass('description').style.backgoundColor = "Blue";
-        //  $('.description').css('background-color', 'Green');
-     } else if (currentHour < blockHour) {
-         document.getElementsByClass('description').style.backgoundColor = "Red";
-        //  $('.description').css('background-color', 'Red');
-     } else {
-         document.getElementsByClass('description').style.backgoundColor = "Orange";
-        //  $('.description').css('background-color', 'White');
-     }
-
-    //  document.getElementsByClass('description').style.backgoundColor = "green";
- };
-
-checkTime;
-
-//  function setColor(element, color) {
-//      element.style.backgoundColor = color;
-//  }
-
 
 var displayHour = 9
 var displayampm = "am"
+var currentHour = moment().format('H') - 9;
 
 for (var i = 0; i < workH; i++) {
     if (displayHour === 13) {
@@ -82,18 +18,35 @@ for (var i = 0; i < workH; i++) {
     var existingV = localStorage.getItem(`hour-${i}`)
     if (existingV !== undefined) {
         textcontent = existingV;
-    } 
+    }
     if (existingV == undefined) {
         textcontent = "";
-    } 
-    
-    container.append(`
+    }
+    if (currentHour == i) {
+        container.append(`
   <div class="row time-block">  
     <div class="col-md-1 hour">${displayHour}${displayampm}</div>
-    <textarea class="col-md-10 description" id="${i}">${textcontent}</textarea>
+    <textarea class="col-md-10 present description" id="${i}">${textcontent}</textarea>
     <button class="col-md-1 saveBtn" id="edit-${i}" data-hour="Cheese" data-id="${i}">💾</button>
   </div>
 `)
+    } else if (currentHour > i) {
+        container.append(`
+    <div class="row time-block">  
+      <div class="col-md-1 hour">${displayHour}${displayampm}</div>
+      <textarea class="col-md-10 past description" id="${i}">${textcontent}</textarea>
+      <button class="col-md-1 saveBtn" id="edit-${i}" data-hour="Cheese" data-id="${i}">💾</button>
+    </div>
+  `)
+    } else {
+        container.append(`
+    <div class="row time-block">  
+      <div class="col-md-1 hour">${displayHour}${displayampm}</div>
+      <textarea class="col-md-10 future description" id="${i}">${textcontent}</textarea>
+      <button class="col-md-1 saveBtn" id="edit-${i}" data-hour="Cheese" data-id="${i}">💾</button>
+    </div>
+  `)
+    }
     displayHour++;
 }
 
